de9af8baace54d42434e6a3862d79aeb6f2d18ee
riteshkumardev <sriteshkumar11022@gmail.com>
2023-03-29 20:55:27 +0530

    messengers

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -69,6 +69,7 @@ function Header() {
               textAlign: "center",
               display: "flex",
               justifyContent: "space-between",
+              marginLeft: "20px",
             }}
           >
             {authenticated
