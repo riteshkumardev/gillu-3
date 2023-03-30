dfe73147e7ea8197ef92255128da9f0820f9b3c8
riteshkumardev <sriteshkumar11022@gmail.com>
2023-03-30 04:36:19 -0400

    check

--- src/actions/auth.actions.js
+++ src/actions/auth.actions.js
@@ -70,7 +70,7 @@ export const signup = (user) => {
           payload: true,
         });
 
-        console.log(error, "gssg");
+        console.log(error);
       });
   };
 };
--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -174,7 +174,7 @@ const HomePage = (props) => {
                       height: "40px",
                       borderRadius: "40px",
                       margin: "auto",
-                      bottom: "7%",
+                      bottom: "3%",
 
                       position: "fixed",
                       marginRight: "17px",
